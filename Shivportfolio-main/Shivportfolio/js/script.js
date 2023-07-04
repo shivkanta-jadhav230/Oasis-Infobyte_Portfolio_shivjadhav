d49b139344eb5f85d63112c81f8dcd581c781139
shivkanta-jadhav230 <132249188+shivkanta-jadhav230@users.noreply.github.com>
2023-07-04 07:50:50 +0530

    Add files via upload

---
+++ Shivportfolio-main/Shivportfolio/js/script.js
@@ -0,0 +1,33 @@
+$(document).ready(function(){
+
+    $('#menu').click(function(){
+      $(this).toggleClass('fa-times');
+      $('header').toggleClass('toggle');
+    });
+  
+    $(window).on('scroll load',function(){
+  
+      $('#menu').removeClass('fa-times');
+      $('header').removeClass('toggle');
+  
+      if($(window).scrollTop() > 0){
+        $('.top').show();
+      }else{
+        $('.top').hide();
+      }
+  
+    });
+  
+
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+      anchor.addEventListener('click', function (e) {
+          e.preventDefault();
+          document.querySelector(this.getAttribute('href')).scrollIntoView({
+              behavior: 'smooth'
+          });
+      });
+  });
+  
+  });
+
+ 
